9e50ce9056342129f4c7b9489c02e9881b769d52
hamotskyi <artem.hamotskyi@gmail.com>
2022-04-15 22:59:24 +0300

    function addNewBank

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 let bankList = [];
+
 let titlesWrap = document.getElementById('titlesWrapper');
-let descriptionWrap = document.getElementById('descriptionWrapper')
+let descriptionWrap = document.getElementById('descriptionWrapper');
+let listBankSettings = document.getElementById('listBankSettings');
+let divBankName = document.getElementById('divBankName');
+let divNearBankName = document.getElementById('divNearBankName');
+let divInterestRate = document.getElementById('divInterestRate');
+let divNearInterestRate = document.getElementById('divNearInterestRate');
+let divMaximumLoan = document.getElementById('divMaximumLoan');
+let divNearMaximumLoan = document.getElementById('divNearMaximumLoan');
+let divMinimumDownPayment = document.getElementById('divMinimumDownPayment');
+let divNearMinimumDownPayment = document.getElementById('divNearMinimumDownPayment');
+let divLoanTerm = document.getElementById('divLoanTerm');
+let divNearLoanTerm = document.getElementById('divNearLoanTerm');
+let divSubmit = document.getElementById('divSubmit');
+let divCalculateMortgage = document.getElementById('divCalculateMortgage');
+
 
 
 class Bank {
@@ -13,8 +28,34 @@ class Bank {
     }
 }
 
+function createNewBank() {
+    let addNewBank = document.getElementById('addNewBank');
+    let inputBankName = document.createElement("input");
+    let inputInterestRate = document.createElement("input");
+    let inputMaximumLoan = document.createElement("input");
+    let inputMinimumDownPayment = document.createElement("input");
+    let inputLoanTerm = document.createElement("input");
+    listBankSettings.style.display = "grid";
+    divNearBankName.appendChild(inputBankName);
+    divNearInterestRate.appendChild(inputInterestRate);
+    divNearMaximumLoan.appendChild(inputMaximumLoan);
+    divNearMinimumDownPayment.appendChild(inputMinimumDownPayment);
+    divNearLoanTerm.appendChild(inputLoanTerm);
+    addNewBank.remove();
+
+}
+
 function startLoad() {
-    
+    let div = document.createElement("div");
+    let attId = document.createAttribute("id");
+    let attOnclick = document.createAttribute('onclick')
+    let textAddNewBank = document.createTextNode("add new bank")
+    attId.value = "addNewBank";
+    attOnclick.value = "createNewBank()"
+    titlesWrap.appendChild(div);
+    div.setAttributeNode(attId);
+    div.setAttributeNode(attOnclick);
+    div.appendChild(textAddNewBank);
 }
 
 document.addEventListener("DOMContentLoaded", startLoad)
